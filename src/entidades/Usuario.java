7ba4420d2924b2e9b7b9731533c8896675c69a76
JoseZapatar <111943481+JoseZapatar@users.noreply.github.com>
2022-11-20 02:14:55 -0600

    Añado clase Usuario

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -12,4 +12,30 @@ public class Usuario extends Persona{
     private int id;
     private String password;
 
+    public Usuario() {
+    }
+
+    public Usuario(int id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+
 }
