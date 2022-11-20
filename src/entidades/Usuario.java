c580cfbc9ae5df8e6819b18828b4954f61bd9c7b
joreims <jorge.garcia14624@est.uca.edu.ni>
2022-11-20 01:50:40 -0600

    Prueba

---
+++ src/entidades/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author jegar
+ */
+public class Usuario extends Persona{
+    private int id;
+    private String password;
+
+}
