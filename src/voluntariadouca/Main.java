40bd8d71c473e718034c9f93c390ee367369b5c7
joreims <jorge.garcia14624@est.uca.edu.ni>
2022-11-28 22:44:06 -0600

    LOGIN PIOLA

--- src/voluntariadouca/Main.java
+++ src/voluntariadouca/Main.java
@@ -4,7 +4,7 @@
  */
 package voluntariadouca;
 
-import presentacion.FrmInicioSesion;
+import presentacion.FrmLogin;
 
 
 /**
@@ -18,7 +18,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        FrmInicioSesion frm = new FrmInicioSesion();
+        FrmLogin frm = new FrmLogin();
         frm.setVisible(true);
     }
     
